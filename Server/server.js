ea2e50d194c301cacc2ea06a8437971e4539469e
Vishal Donga <vishaldonga811@gmail.com>
2021-10-03 09:17:42 +0530

    Update Server file

--- Server/server.js
+++ Server/server.js
@@ -38,9 +38,13 @@ const port = process.env.PORT || 4000;
 // Routes Configuration
 app.use("/api/scores", scoreRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Staring our express server
 const server = app.listen(port, function () {
